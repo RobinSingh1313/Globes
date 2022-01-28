a1837988d7233c4480fab56985aa29f5c4d692ad
RobinSingh1313 <robinsingh13132@gmail.com>
2022-01-28 12:00:47 +0530

    added complete calendar work

--- globewater/lib/activity/body.dart
+++ globewater/lib/activity/body.dart
@@ -1,4 +1,6 @@
 import 'dart:ui';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -16,6 +18,18 @@ class Body extends StatefulWidget {
 
 class _BodyState extends State<Body> {
   var _razorpay = Razorpay();
+  var whichfloor;
+  var mobilenumber="9014473447";
+  late DatabaseReference _databaseReference, _anotherdatabaseReference;
+  var currentday;
+   double latitudes=0.0,longitudes=0.0;
+  
+
+  var currentmonth;
+  var k;
+  var now = new DateTime.now();
+  List<String> result = [];
+  List<String> datess = [];
   var _selected = "";
   var options = {
     'key': 'rzp_test_CQvve50jQO7JHM',
@@ -27,6 +41,11 @@ class _BodyState extends State<Body> {
   @override
   void initState() {
     // TODO: implement initState
+    _databaseReference =
+        FirebaseDatabase.instance.ref().child("UserBookRequest");
+    _databaseReference =
+        FirebaseDatabase.instance.ref().child("UserBookedRequest");
+
     _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
     _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
     _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
@@ -54,6 +73,14 @@ class _BodyState extends State<Body> {
     String location = 'Null, Press Button';
     String Address = 'search';
     DateTime selectedDate = DateTime.now();
+
+    currentday = selectedDate.day;
+    currentmonth = selectedDate.month;
+    if (currentmonth >= 10) {
+      k = currentday.toString() + "-" + currentmonth.toString();
+    } else {
+      k = currentday.toString() + "-0" + currentmonth.toString();
+    }
 
 //payment ateway
 
@@ -90,6 +117,8 @@ class _BodyState extends State<Body> {
     Future<void> GetAddressFromLatLong(Position position) async {
       List<Placemark> placemarks =
           await placemarkFromCoordinates(position.latitude, position.longitude);
+          latitudes=position.latitude;
+          longitudes=position.longitude;
       print(placemarks);
       Placemark place = placemarks[0];
       Address =
@@ -176,6 +205,8 @@ class _BodyState extends State<Body> {
                                 items: items.map((String items) {
                                   return DropdownMenuItem(
                                     value: items,
+                                    
+
                                     child: Text(items),
                                   );
                                 }).toList(),
@@ -183,6 +214,7 @@ class _BodyState extends State<Body> {
                                 // change button value to selected value
                                 onChanged: (String? newValue) {
                                   setState(() {
+                                    whichfloor=newValue;
                                     dropdownvalue = newValue!;
                                   });
                                 },
@@ -308,8 +340,21 @@ class _BodyState extends State<Body> {
   }
 
   void _onSelectionChanged(
-      DateRangePickerSelectionChangedArgs
-          dateRangePickerSelectionChangedArgs) {}
+      DateRangePickerSelectionChangedArgs dateRangePickerSelectionChangedArgs) {
+    var list = dateRangePickerSelectionChangedArgs.value.toString();
+    result = list.split(',');
+
+    Fluttertoast.showToast(
+        msg: "P" + result.toString(),
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+    print(list);
+  }
+  //dialog box method
 
   _displayDialog(BuildContext context) async {
     _selected = await showDialog(
@@ -324,13 +369,20 @@ class _BodyState extends State<Body> {
                 width: 250,
                 child: SfDateRangePicker(
                   view: DateRangePickerView.month,
+                  onSelectionChanged: _onSelectionChanged,
                   selectionMode: DateRangePickerSelectionMode.multiple,
                   selectionColor: HexColor('#14446D'),
                 ),
               )),
               SimpleDialogOption(
                 child: ElevatedButton(
-                    onPressed: () => null, child: Text("Select Days")),
+                    onPressed: () => selectDays(), child: Text("Select Days")),
+              ),
+              SimpleDialogOption(
+                child: Text(
+                  "Booking Days:" + datess.toString(),
+                  style: TextStyle(fontSize: 12),
+                ),
               ),
               SimpleDialogOption(
                 child: ElevatedButton(
@@ -350,6 +402,70 @@ class _BodyState extends State<Body> {
         _selected = _selected;
       });
     }
+  }
+
+  selectDays() {
+    datess.clear();
+
+    Fluttertoast.showToast(
+        msg: "P" + result.length.toString(),
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+
+    for (var i = 0; i < result.length; i++) {
+      var check = result[i].substring(9, 11) + "-" + result[i].substring(6, 8);
+      var dateofbooks = result[i].substring(9, 11);
+      int dateofbookss = int.parse(dateofbooks);
+      var bookofmonth = result[i].substring(6, 8);
+      int bookofmonths = int.parse(bookofmonth);
+      print(bookofmonths.runtimeType);
+      print(dateofbookss.runtimeType);
+      print(currentmonth.runtimeType);
+      print(currentday.runtimeType);
+
+      if (currentmonth >= bookofmonths) {
+        if (dateofbookss >= currentday) {
+          datess.add(check);
+        }
+      } else {
+        Fluttertoast.showToast(
+            msg: "Sorry U cannot book past days",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      }
+    }
+    Fluttertoast.showToast(
+        msg: "Bookin Days:" + datess.toString(),
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+    print("hello" + datess.toString());
+  }
+
+  void BookOrder(String paymentType) {
+    Map<String, String> contact = {
+      'Delivery':'false',
+      'days' : 'dates',//add
+      'floor':whichfloor,
+      'latitude':latitudes.toString(),
+      'longitude':longitudes.toString(),
+      'paymentmode':paymentType,
+      'quantitys':
+      'total':
+      'uid':mobilenumber
+
+    };
   }
 }
 
